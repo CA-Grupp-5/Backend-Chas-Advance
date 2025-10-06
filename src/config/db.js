49c8a68e07a1f378fa1c3275974bf8650501e1bc
filipwretner <filip.wretner@chasacademy.se>
2025-10-06 14:11:19 +0200

    Latest Azure fix

--- src/config/db.js
+++ src/config/db.js
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
+  host: process.env.DB_SERVER,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false } // Azure PSQL kräver SSL
+  ssl: { rejectUnauthorized: false }, // Azure PSQL kräver SSL
 });
 
 export default pool;
