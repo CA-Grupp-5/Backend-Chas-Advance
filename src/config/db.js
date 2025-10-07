1c1251b0a253d7a37b810fab6b32777eb77dc39d
filipwretner <filip.wretner@chasacademy.se>
2025-10-07 10:43:12 +0200

    Testing env

--- src/config/db.js
+++ src/config/db.js
@@ -4,22 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: 'team-5-pg-server.postgres.database.azure.com',
-  port: 5432,
-  user: 'team5',
-  password: 'ChasAdvance123098',
-  database: 'team-5-db',
+  host: process.env.DB_SERVER,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: { rejectUnauthorized: false }, // Azure PSQL kräver SSL
 });
-
-// const pool = new Pool({
-//   host: process.env.DB_SERVER,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: 'ChasAdvance123098',
-//   database: process.env.DB_NAME,
-//   ssl: { rejectUnauthorized: false }, // Azure PSQL kräver SSL
-// });
 
 // async function test() {
 //   try {
