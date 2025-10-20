7d46458c579a3cff9aa6eddce5e8852d42e787fe
filipwretner <filip.wretner@chasacademy.se>
2025-10-20 14:57:20 +0200

    Syntax fix

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 // src/server.js
-import dotenv from dotenv;
+import dotenv from 'dotenv';
 import app from './app.js';
 import logger from './utilities/logger.js';
 
@@ -8,7 +8,11 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  logger.info(`🚀 Dev server running on http://localhost:${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
+  logger.info(
+    `🚀 Dev server running on http://localhost:${PORT} in ${
+      process.env.NODE_ENV || 'development'
+    } mode`
+  );
   console.log(
     `Server is running on http://g5app-ctepc9hrehedf2fw.swedencentral-01.azurewebsites.net`
   );
