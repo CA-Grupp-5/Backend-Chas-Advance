f921e1f56fe47a192c2724bb8a2bff8c4a2fecaf
filipwretner <filip.wretner@chasacademy.se>
2025-10-28 10:51:07 +0100

    Added allowed domains to swagger documentation

--- src/utilities/swaggerConfig.js
+++ src/utilities/swaggerConfig.js
@@ -1,5 +1,10 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import allowedCompanies from '../config/allowedDomains.json' with { type: 'json' };
+
+const allowedDomains = allowedCompanies.map(
+  (entry) => `- **${entry.company}** - \`${entry.domain}\``)
+  .join('\n');
 
 const options = {
   definition: {
@@ -7,7 +12,17 @@ const options = {
     info: {
       title: 'API Documentation',
       version: '1.0.0',
-      description: 'API documentation for Group 5 Chas Advance Project.',
+      description: `
+### Group 5 Chas Advance Project API
+
+This API allows user registration and authentication.
+
+#### Allowed Email Domains for Registration
+
+Only users with email addresses belonging to the following company domains may register:
+
+${allowedDomains}
+      `,
     },
   },
   apis: ['./src/routes/**/*.js'],
